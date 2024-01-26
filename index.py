18d65ba94cd814aeeffc57b5e1085dccf80bcf0e
Santiago <97931471+Santiago1820@users.noreply.github.com>
2024-01-26 23:46:14 +0000

    Pending changes exported from your codespace

---
+++ index.py
@@ -0,0 +1,7 @@
+username = input("¿Cuál es tu nombre?")
+userbirthday = input("¿Cuál es tu dia de cumpleaños?")
+usermonth = input("¿Cuál es tu mes de cumpleaños?")
+usseryear = input("¿Cuál es tu año de nacimiento?")
+
+print("Hola, " + username)
+
