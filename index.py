e6c05793e128923c76b425183978bb50aac08aeb
Santiago <97931471+Santiago1820@users.noreply.github.com>
2024-01-30 23:19:50 +0000

    index.py index.py

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
+from datetime import date
 username = input("¿Cuál es tu nombre?")
-userbirthday = input("¿Cuál es tu dia de cumpleaños?")
-usermonth = input("¿Cuál es tu mes de cumpleaños?")
-usseryear = input("¿Cuál es tu año de nacimiento?")
+userbirthday = int(input("¿Cuál es tu dia de cumpleaños?"))
+usermonth = int(input("¿Cuál es tu mes de cumpleaños?"))
+useryear = int(input("¿Cuál es tu año de nacimiento?"))
 
 print("Hola, " + username)
 
+edad = (date.today() - date(useryear, usermonth, userbirthday)).days // 365
+
+print (edad)
