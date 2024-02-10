1e6d4ac417c127ba3ea89ae8642c57e96176ca85
Santiago <97931471+Santiago1820@users.noreply.github.com>
2024-02-10 01:09:21 +0000

    Correccion fecha

--- index.py
+++ index.py
@@ -6,15 +6,14 @@ username = input("¿Cuál es tu nombre? ")
 userbirthday = int(input("¿Cuál es tu dia de cumpleaños? "))
 usermonth = int(input("¿Cuál es tu mes de cumpleaños? "))
 useryear = int(input("¿Cuál es tu año de nacimiento? "))
-print("Hola, ", username)
+print("Hola,", username)
 
 # Declaramos nuestra variable edad apra obtener su edad
-edad = (date.today() - date(useryear, usermonth, userbirthday)).days // 365
-edadd = edad * 12
-edadm = edad * 12
-print ("Tu edad es: ", edad, " años")
-print ("Han pasado: ", edadd, " días")
-print ("Han pasado: ", edadm, " meses")
+hoy = (date.today())
+edad = (hoy - date(useryear, usermonth, userbirthday)).days // 365
+edadm = hoy.month
+edadd = hoy.day - 1
+print ("Han pasado:", edad, "años", edadm, "meses", "y", edadd, "días desde que nacise hasta hoy: ", f"{edadd}/{edadm}/{hoy.year}")
 
 #Traer el signo zoodiacal
 print ("Tu signo es: ", zood.sigzood(userbirthday, usermonth))
