b5d3e0dc7783fbd899417d08cb18fd19e93e41ec
Santiago <97931471+Santiago1820@users.noreply.github.com>
2024-02-17 00:37:02 +0000

    Borrado de lineas

--- banco.py
+++ banco.py
@@ -1,25 +1,24 @@
 inicial = 0 
-cantiafaf = 0
 ahorro = 0
 objetivo = int(input("Cual es tu objetivo: "))
 
 while inicial < 1:
-    print ("Tu saldo es: ", ahorro)
-    print ("selecciona una opcion:")
-    print("1: ingresar dinero")
-    print("2: retirar dinero")
-    print("3: Salir")
+    print ("Tu saldo es: ", ahorro, "\nSelecciona una opcion:\n1: ingresar dinero\n2: retirar dinero\n3: Salir")
     opcion = input()
     if opcion == "1":
         cantiadad = int(input ("Cuanto deseas añadir: ")) 
         ahorro = ahorro + cantiadad
     elif opcion == "2":
         cantiadad = int(input ("Cuanto deseas retirar: ")) 
-        ahorro = ahorro - cantiadad
-    if opcion == "3":
-        break
-    if ahorro == objetivo:
-        print("Has llegado a tu meta",objetivo)
+        if cantiadad > ahorro:
+            print("No puedes retirar más de lo que tienes")
+        else:
+            ahorro = ahorro - cantiadad
+    elif opcion == "3":
         break
     else:
         print("Esa opción no exite selecciona una opción correcta: ")
+    
+    if ahorro >= objetivo:
+        print("Has llegado a tu meta",objetivo)
+        break      
